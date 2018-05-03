84c265c2d72e43c797b5ab3b3ada108725153418
student35 <student35@student35.local>
2018-05-03 20:59:11 +0900

    first

---
+++ Match App/CardCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CardCollectionViewCell.swift
+//  Match App
+//
+//  Created by student35 on 2018. 5. 3..
+//  Copyright © 2018년 student35. All rights reserved.
+//
+
+import UIKit
+
+class CardCollectionViewCell: UICollectionViewCell {
+    
+    
+}
